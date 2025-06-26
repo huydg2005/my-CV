2833ac1e5913f642e47abd60b3b7283bd258002d
huydg2005 <youngduonghuy@gmail.com>
2025-06-26 09:51:42 +0700

    done

--- script.js
+++ script.js
@@ -17,3 +17,38 @@ const appearOnScroll = new IntersectionObserver((entries, observer) => {
 fadeIns.forEach(fader => {
   appearOnScroll.observe(fader);
 });
+window.addEventListener('scroll', () => {
+  const home = document.getElementById('Home');
+  if (window.scrollY > 100) {
+    home.classList.add('intro-hidden');
+  } else {
+    home.classList.remove('intro-hidden');
+  }
+});
+  const scrollTopBtn = document.querySelector('.scroll-to-top');
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 300) {
+      scrollTopBtn.classList.add('show');
+    } else {
+      scrollTopBtn.classList.remove('show');
+    }
+  });
+
+  document.addEventListener('DOMContentLoaded', function () {
+    const elements = document.querySelectorAll('.fade-in, .fade-in-left, .fade-in-right');
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('appear');
+        } else {
+          entry.target.classList.remove('appear'); // Khi ra khỏi khung nhìn thì ẩn lại
+        }
+      });
+    }, { threshold: 0.3 });
+
+    elements.forEach(el => observer.observe(el));
+  });
+
+
